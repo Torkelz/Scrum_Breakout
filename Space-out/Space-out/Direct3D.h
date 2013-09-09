3c19225e0c1d587cea1ef2c95c12c862d927b2ca
Torkelz <and_re1@hotmail.com>
2013-09-09 12:20:17 +0200

    Clean
    Clean DirectX project med message loop, draw, update, init metoder
    unrolled.

---
+++ Space-out/Space-out/Direct3D.h
@@ -0,0 +1,33 @@
+#ifndef DIRECT3D_H
+#define DIRECT3D_H
+
+#include "d3dApp.h"
+#include <DirectXPackedVector.h>
+#include <string>
+
+using namespace DirectX;
+ 
+class Direct3D : public D3DApp
+{
+public:
+	Direct3D(HINSTANCE hInstance);
+	~Direct3D();
+
+	void initApp();
+	void onResize();
+	void updateScene(float dt);
+	void drawScene();
+
+	LRESULT msgProc(UINT msg, WPARAM wParam, LPARAM lParam);
+private:
+	//Camera Variables
+	XMMATRIX camView;
+	XMMATRIX camProjection;
+	
+	XMVECTOR camPosition;
+	XMVECTOR camTarget;
+	XMVECTOR camUp;
+
+	int test;
+};
+#endif
