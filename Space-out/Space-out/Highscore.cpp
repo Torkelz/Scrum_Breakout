1b655026ede271d919d386735e06e9cd16f43922
Christian Lindås <christian.lindas@hotmail.com>
2013-10-17 09:50:48 +0200

    Added Class

---
+++ Space-out/Space-out/Highscore.cpp
@@ -0,0 +1,23 @@
+#include "Highscore.h"
+
+Highscore::Highscore()
+{
+
+}
+
+Highscore::~Highscore(){}
+
+void Highscore::loadHighscoreFromFile()
+{
+
+}
+
+void Highscore::saveHighscoreToFile()
+{
+
+}
+
+void Highscore::getHighScore()
+{
+
+}
